f8cc9e37c50c2bbfcc38efe74a604694a988d973
Bazaid Khan <bazaid-k@hotmail.com>
2021-02-25 12:17:53 +0300

    .

---
+++ products.js
@@ -0,0 +1,29 @@
+let products = [
+    {
+      "id": 1001,
+      "title": "Cat Food, 25lb bag",
+      "description": "A 25 pound bag of <em>irresistible</em>, organic goodness for your cat.",
+      "price": 20.00,
+      "image": "catfood.jpg",
+      "availableInventory": 10,
+      "rating": 2
+    },
+    {
+      "id": 1002,
+      "title": "Yarn",
+      "description": "Yarn your cat can play with for a very <strong>long</strong> time!",
+      "price": 2.99,
+      "image": "yarn.jpg",
+      "availableInventory": 7,
+      "rating": 3
+    },
+    {
+      "id": 1003,
+      "title": "Laser Pointer",
+      "description": "Drive your cat crazy with this <em>amazing</em> product.",
+      "price": 49.99,
+      "image": "laser.jpg",
+      "availableInventory": 25,
+      "rating": 4
+    }
+  ]
